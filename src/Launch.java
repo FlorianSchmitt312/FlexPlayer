e394b76f42e684df68831934fb1dc7f63ce86a5d
flunschi <floschmitt95@yahoo.de>
2020-09-23 19:34:58 +0200

    Added a Demo Window and a Startup Class

---
+++ src/Launch.java
@@ -0,0 +1,16 @@
+import javafx.application.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Launch {
+	
+	public static void  main (String args []) throws Exception 
+	{
+		Application.launch(PlayerStart.class, args);
+	}
+
+}
